4e0a9c580de53499a81b27b67cc7172e703573f1
Wes Cutting <wesleycutting@gmail.com>
2020-05-18 15:37:20 -0600

    Added Register Encryption at REST

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,29 @@
 var express = require('express');
+const bcrypt = require('bcrypt');
 var router = express.Router();
 const { registerUser } = require('../../dal/auth');
 
-router.post('/register', async function (req, res) {
+const salt = process.env.SALT;
+
+router.post('/register', function (req, res) {
     try {
-        const data = await registerUser(req.body);
-        res.send(data);
+        const saltRounds = +salt;
+        const body = req.body;
+        const myPlaintextPassword = body.password;
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if(err){
+                throw err;
+            }
+            bcrypt.hash(myPlaintextPassword, salt, async function (err, hash) {
+                if (err) {
+                    throw err;
+                }
+                body.password = hash;
+                const data = await registerUser(req.body);
+                res.send(data);
+            });
+        });
+        
     } catch (err) {
         console.log(err);
         res.status(500).send('Internal Server Issue, Check Server Logs');
